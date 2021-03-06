2bd90eb494fc165cd6d077e0bd88be226c41742c
HugoMatilla <hugomatilla@gmail.com>
2021-03-06 21:40:16 +0100

    Add gesture input

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -19,13 +19,19 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.LinearProgressIndicator
@@ -39,8 +45,10 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.Modifier.Companion
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign.Center
@@ -48,10 +56,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
-lateinit var input: MutableState<TextFieldValue>
 lateinit var timer: CountDownTimer
 lateinit var counter: MutableState<Int>
-lateinit var max: MutableState<Int>
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -66,85 +72,120 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun MyApp() {
+  val output = remember { mutableStateOf(0) }
   Surface(color = MaterialTheme.colors.background) {
     Column(
       modifier = Modifier.fillMaxSize(),
       verticalArrangement = Arrangement.SpaceAround
     ) {
-      Clock()
-      Buttons()
+      InputTimer(output)
+      Clock(output)
+      Buttons(output)
     }
   }
 }
 
 @Composable
-fun Buttons() {
+fun InputTimer(output: MutableState<Int>) {
+  var input by remember { mutableStateOf(0f) }
+  Box(
+    Modifier
+      .size(150.dp)
+      .scrollable(
+        orientation = Orientation.Vertical,
+        state = rememberScrollableState { delta ->
+          input = getInputFromDelta(delta, input)
+          output.value = input.getNumberFromOffset()
+          delta
+        }
+      )
+      .background(Color.LightGray),
+    contentAlignment = Alignment.Center
+  ) {
+    Text(
+      output.value.toString(),
+      style = MaterialTheme.typography.h3
+    )
+  }
+}
+
+private fun Float.getNumberFromOffset() = (if (this < 0) 60 + this else this).toInt()
+
+fun getInputFromDelta(delta: Float, number: Float): Float {
+  var result = number
+  when {
+    result > number + delta -> result = (number + 0.25f) % 60
+    result < number + delta -> result = (number - 0.25f) % 60
+  }
+  return result
+}
+
+@Composable
+fun Buttons(output: MutableState<Int>) {
   Row(
     modifier = Modifier.fillMaxSize(),
     horizontalArrangement = Arrangement.SpaceAround
   ) {
-    Button(onClick = { startTimer() }) { Text(text = "Start") }
+    Button(onClick = { startTimer(output) }) { Text(text = "Start") }
     Button(onClick = { stopTimer() }) { Text(text = "Stop") }
     Button(onClick = { stopTimer(); counter.value = 0 }) { Text(text = "cancel") }
   }
 }
 
 @Composable
-fun Clock() {
+fun Clock(output: MutableState<Int>) {
   counter = remember { mutableStateOf(0) }
-  max = remember { mutableStateOf(1) }
   Column(
     modifier = Modifier.height(320.dp),
     verticalArrangement = Arrangement.SpaceEvenly
   ) {
-    Input()
     Text(
       text = "Time ${counter.value}", style = MaterialTheme.typography.h3,
       modifier = Modifier.fillMaxWidth(),
       textAlign = Center
-
     )
-    LinearProgressIndicator(
-      modifier = Modifier.fillMaxWidth(),
-      progress = (counter.value.toFloat() / max.value.toFloat())
-    )
+    if (output.value > 0)
+      LinearProgressIndicator(
+        modifier = Modifier.fillMaxWidth(),
+        progress = (counter.value.toFloat() / output.value.toFloat())
+      )
   }
 }
-
-@Composable
-fun Input() {
-  input = remember { mutableStateOf(TextFieldValue()) }
-  TextField(
-    value = input.value,
-    onValueChange = { new ->
-      input.value = new
-      counter.value = if (new.text.isBlank()) 1 else new.text.toInt()
-      max.value = if (new.text.isBlank()) 1 else new.text.toInt()
-    },
-    singleLine = true,
-    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-    modifier = Modifier.fillMaxWidth()
-  )
-}
+//
+//@Composable
+//fun Input() {
+//  input = remember { mutableStateOf(TextFieldValue()) }
+//  TextField(
+//    value = input.value,
+//    onValueChange = { new ->
+//      input.value = new
+//      counter.value = if (new.text.isBlank()) 1 else new.text.toInt()
+////      output.value = if (new.text.isBlank()) 1 else new.text.toInt()
+//    },
+//    singleLine = true,
+//    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+//    modifier = Modifier.fillMaxWidth()
+//  )
+//}
 
 fun stopTimer() {
   timer.cancel()
 }
 
-fun startTimer() {
-  updateTimer(if (input.value.text.isBlank()) 0 else input.value.text.toInt())
+fun startTimer(output: MutableState<Int>) {
+  updateTimer(output)
   timer.start()
 }
 
-fun updateTimer(newCounterValue: Int) {
-  counter.value = newCounterValue + 1
-  timer = object : CountDownTimer(newCounterValue * 1_000L, 1_000) {
+fun updateTimer(output: MutableState<Int>) {
+  counter.value = output.value + 1
+  timer = object : CountDownTimer(output.value * 1_000L, 1_000) {
     override fun onTick(millisUntilFinished: Long) {
       counter.value = counter.value - 1
     }
 
     override fun onFinish() {
-      counter.value = newCounterValue
+      counter.value = output.value
     }
   }
 }
@@ -155,14 +196,6 @@ private class Time(hours: State<Int>, minutes: State<Int>, seconds: State<Int>, 
   val minutes by minutes
   val seconds by seconds
   val millis by millis
-}
-
-@Composable
-private fun FancyClock(time: Time) {
-  Row {
-    Text(text = "${time.hours}:${time.minutes}:${time.seconds}")
-//    Text(text = "${time.hours}:${time.minutes}:${time.seconds}:${time.millis}:")
-  }
 }
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
